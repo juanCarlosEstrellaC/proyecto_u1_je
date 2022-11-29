31f94b264556e119514a10f5b05490b081841311
Juan Estrella <jcestrella@uce.edu.ec>
2022-11-28 19:46:11 -0500

    Taller9: componentes de negocio

---
+++ src/main/java/com/example/demo/banco/modelo/CuentaBancaria.java
@@ -0,0 +1,41 @@
+package com.example.demo.banco.modelo;
+
+import java.math.BigDecimal;
+
+public class CuentaBancaria {
+
+	private String numero; // cuando un dato no es sujeto de operaciones matemáticas, no tiene sentido que sea Integer o Double.
+	private String tipo;
+	private BigDecimal saldo; // para datos monetarios
+	private String titular;		
+	
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	
+	
+	
+	
+}
